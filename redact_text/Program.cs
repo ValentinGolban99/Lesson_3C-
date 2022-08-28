b6dd3935b92e06b86516b93c1097d19723cf74e4
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-08-28 19:13:59 +0300

    Ели ели наладил код))

---
+++ redact_text/Program.cs
@@ -0,0 +1,30 @@
+﻿string text = "— Я думаю, — сказал князь, улыбаясь, — что, ежели бы вас послали вместо нашего милого Винценгероде, вы бы взяли приступом согласие прусского короля. Вы так красноречивы. Вы дадите мне чаю?";
+
+// string Replase(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $”{NewValue}”;
+//         else result + result $"{text[i]}";
+//     }
+//     return result
+// }
+
+// string newText = replace(text, ' ', ' | ');
+// Console.WriteLine(newText);
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+Console.WriteLine(newText);
