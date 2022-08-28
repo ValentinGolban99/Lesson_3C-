724eac3f0833829dc9371962726bec7e44c7cb50
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-08-28 16:48:35 +0300

    metod 3

---
+++ list_4/Program.cs
@@ -0,0 +1,13 @@
+﻿string Metod4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i<count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Metod4(10, "z");
+Console.WriteLine(res);
